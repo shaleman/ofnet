15d5d5ab693244b13694158de24cce2d6c507d74
Sukhesh Halemane <sukhesh_h@yahoo.com>
2016-04-05 20:27:46 -0700

    Fix sanity failure

--- vrouter.go
+++ vrouter.go
@@ -314,10 +314,6 @@ func (self *Vrouter) AddVtepPort(portNo uint32, remoteIp net.IP) error {
 
 		//set vrf id as METADATA
 		vrfmetadata, vrfmetadataMask := Vrfmetadata(*vrfid)
-
-		metadata := METADATA_RX_VTEP | vrfmetadata
-		metadataMask := METADATA_RX_VTEP | vrfmetadataMask
-
 		portVlanFlow.SetMetadata(metadata, metadataMask)
 
 		// Point it to next table.
